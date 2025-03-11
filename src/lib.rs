05e671596670125aac008e6ef2204e37c14d3a4d
Oscar Roa <oscaroa196@gmail.com>
2025-03-10 19:29:55 -0600

    chore: lint and fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,13 @@
-use std::net::TcpListener;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health-check", web::get().to(health_check))
-    })
+    let server = HttpServer::new(|| App::new().route("/health-check", web::get().to(health_check)))
         .listen(listener)?
         .run();
 
