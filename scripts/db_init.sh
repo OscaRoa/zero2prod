8e1bcfae301090c2e2f19353df5342ba40ae7c6b
Oscar Roa <11345652+OscaRoa@users.noreply.github.com>
2025-03-11 23:43:59 -0600

    feature: add migration steps with sqlx to db_init script and first migration

--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -3,39 +3,63 @@ set -x
 set -eo pipefail
 source .env
 
-# Check if a custom parameter has been set, otherwise use default values
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 " cargo install sqlx-cli --no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
+if ! [ -x "$(command -v podman)" ]; then
+  echo >&2 "Error: podman is not installed."
+  echo >&2 "Visit: https://podman.io/docs/installation"
+  echo >&2 " for installation instructions on your environment."
+  exit 1
+fi
+
 DB_PORT="${POSTGRES_PORT}"
 
 # Launch postgres using Docker
-CONTAINER_NAME="postgres"
+if [[ -z "${SKIP_DB_INIT}" ]]
+then
+  CONTAINER_NAME="postgres"
 
-podman run \
-  --env POSTGRES_USER="${SUPERUSER}" \
-  --env POSTGRES_PASSWORD="${SUPERUSER_PWD}" \
-  --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
-  --health-interval="1s" \
-  --health-timeout="5s" \
-  --health-retries=5 \
-  --publish "${DB_PORT}":5432 \
-  --detach \
-  --name "${CONTAINER_NAME}" \
-  --replace \
-  postgres -N 1000
-  
-# Wait for Postgres to be ready to accept connections
-until [ \
-  "$(podman inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
-  "healthy" \
-]; do
-  >&2 echo "Postgres is still unavailable - sleeping"
-  sleep 1
-done
+  podman run \
+    --env POSTGRES_USER="${SUPERUSER}" \
+    --env POSTGRES_PASSWORD="${SUPERUSER_PWD}" \
+    --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+    --health-interval="1s" \
+    --health-timeout="5s" \
+    --health-retries=5 \
+    --publish "${DB_PORT}":5432 \
+    --detach \
+    --name "${CONTAINER_NAME}" \
+    --replace \
+    postgres -N 1000
 
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
-  
-# Create the application user
-CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
-podman exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
-# Grant create db privileges to the app user
-GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
-podman exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
+  # Wait for Postgres to be ready to accept connections
+  until [ \
+    "$(podman inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+    "healthy" \
+  ]; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+  done
+
+  >&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+  # Create the application user
+  CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
+  podman exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
+  # Grant create db privileges to the app user
+  GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
+  podman exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
+fi
+
+
+DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@localhost:${DB_PORT}/${APP_DB_NAME}
+export DATABASE_URL
+sqlx database create
+sqlx migrate run
+>&2 echo "Postgres has been migrated, ready to go!"
