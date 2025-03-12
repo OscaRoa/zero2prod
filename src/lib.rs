323dfe12834fcc253303a7c32b7106b24fe2ce29
Oscar Roa <11345652+OscaRoa@users.noreply.github.com>
2025-03-11 22:44:00 -0600

    feature: add initial subscribe endpoint with form deserialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
 use actix_web::dev::Server;
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 #[get("/health-check")]
-async fn health_check() -> impl Responder {
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[post("/subscriptions")]
+async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check))
+    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
         .listen(listener)?
         .run();
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 use zero2prod::run;
-#[tokio::main]
+
+#[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     run(TcpListener::bind("127.0.0.1:8000").unwrap())?.await
 }
