2c271b19fd12990550a71dcc273a9190058fe9cf
Oscar <11345652+OscaRoa@users.noreply.github.com>
2025-03-16 16:55:29 -0600

    feat: add configuration with config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use std::net::TcpListener;
-use zero2prod::startup::run;
+use zero2prod::{
+    startup::run,
+    configuration::get_configuration,
+};
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    run(TcpListener::bind("127.0.0.1:8000").unwrap())?.await
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -19,10 +21,18 @@ async fn health_check_works() {
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    // Arrange
+    // Preparation steps
     let app_address = spawn_app();
+    let configuration = get_configuration().expect(
+        "Failed to read configuration"
+    );
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to postgres");
     let client = reqwest::Client::new();
-    // Act
+
+    // Running
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
         .post(format!("{}/subscriptions", app_address))
