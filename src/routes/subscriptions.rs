fc79786807a74ba6d7cd928ff55ae9aa4c827a32
Oscar <11345652+OscaRoa@users.noreply.github.com>
2025-03-18 19:10:19 -0600

    feat: add db test assertions
    WIP: Fix permissions on dummy db for tests

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
 use actix_web::{post, web, HttpResponse};
+use sqlx::PgPool;
+use chrono::Utc;
+use uuid::{ContextV7, Timestamp, Uuid};
 
 #[derive(serde::Deserialize)]
 struct FormData {
@@ -7,6 +10,23 @@ struct FormData {
 }
 
 #[post("/subscriptions")]
-async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn subscribe(form: web::Form<FormData>, connection_pool: web::Data<PgPool>) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v7(Timestamp::now(ContextV7::new())),
+        form.email,
+        form.name,
+        Utc::now(),
+    )
+        .execute(connection_pool.get_ref())
+        .await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => { 
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,15 @@
 use std::net::TcpListener;
 use actix_web::dev::Server;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
+use sqlx::{PgPool};
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+
+    let server = HttpServer::new(move || {
+        App::new().service(health_check).service(subscribe).app_data(db_pool.clone())
+    })
         .listen(listener)?
         .run();
 
